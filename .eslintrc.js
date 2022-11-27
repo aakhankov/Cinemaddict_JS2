9f8ab0527247ae86f104096f82621075b9e46a34
aakhankov <anton.akhankov@gmail.com>
2022-11-27 23:10:58 +0100

    module1-task3

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+	node: 1,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+	quotes: [2, "single", { "avoidEscape": true }]
+  },
+};
